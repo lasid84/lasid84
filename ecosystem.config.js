65a1273e6b37a3c0eba59796b11f36686cd054d4
stephen.lim <stephen.lim@kwe.com>
2023-12-11 17:24:07 +0900

    6666

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,16 +19,6 @@ module.exports = {
       env: {
         NODE_ENV: 'production'
       }
-    },
-    {
-      name: 'web',
-      script: 'dist/index.js',
-      cwd: "./apps/api",
-      autorestart: true,    
-      watch: '.',
-      env: {
-        NODE_ENV: 'production'
-      }
     }
   ],
 
@@ -51,7 +41,7 @@ module.exports = {
       repo : 'git@gitlab.kwe.co.kr:sdd_it/kream_web.git',
       path : '/home/sdd_it/KREAM_Web',
       ssh_options: ['StrictHostKeyChecking=no'],
-      'post-deploy' : 'pnpm install && pnpm build && pm2 reload ecosystem.config.js --only batch --env production',
+      'post-deploy' : 'pnpm install && pnpm build && pm2 reload ecosystem.config.js --env production',
     }
     ,
     batch_prod : {
