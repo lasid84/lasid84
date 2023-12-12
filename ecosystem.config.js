7e72f9de6e2c1f4e584783c57da8e212a6ec7049
stephen.lim <stephen.lim@kwe.com>
2023-12-12 15:28:59 +0900

    20

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -65,7 +65,7 @@ module.exports = {
       user : 'sdd_it',
       host : ['10.33.63.172'],
       ref  : 'origin/main',
-      repo : 'git@gitlab.kwe.co.kr/kream_web.git',
+      repo : 'git@gitlab.kwe.co.kr:sdd_it/kream_web.git',
       path : '/home/sdd_it/KREAM_Web',
       ssh_options: ['StrictHostKeyChecking=no'],
       'post-deploy' : 'pnpm install && pnpm build && pm2 reload ecosystem.config.js --env production --only batch',
