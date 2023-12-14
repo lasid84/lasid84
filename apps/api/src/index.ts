0ab8b4251d6492f21f4e0bebdda42983ca771d13
stephen.lim <stephen.lim@kwe.com>
2023-12-14 14:16:20 +0900

    17

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,7 @@
 // import { log, ini, objectPath, fs } from "@repo/kwe-lib";
-// const ini = require("ini");
-// const fs = require("fs");
-// const objectPath = require("objectPath");
+const ini = require("ini");
+const fs = require("fs");
+const objectPath = require("objectPath");
 //import { createServer } from './server.js';
 const {createServer} = require("./server.ts");
 
@@ -13,9 +13,9 @@ async function init () {
     var port = 0;
     const server = createServer();
 
-    // var iniData = ini.decode(await fs.readFile(process.cwd() + "/configs/server.ini", "utf8"));
-    // port = objectPath.get(iniData, "main.port");
-    port = 5005;
+    var iniData = ini.decode(await fs.readFile(process.cwd() + "/configs/server.ini", "utf8"));
+    port = objectPath.get(iniData, "main.port");
+    // port = 5005;
     server.listen(port, () => {
       console.log(`api running on ${port}`);
     });
