0c1dfb45c3eee82e3d93045d7bf34e7160b649f9
doni.lee <doni.lee@kwe.com>
2024-02-07 15:59:59 +0900

    기준정보 폴더생성

--- apps/kream/src/app/stnd/stnd0005/page.tsx
+++ apps/kream/src/app/stnd/stnd0005/page.tsx
@@ -1,8 +1,30 @@
+import PageTitle from "../../../shared/tmpl/page-title"
+import ListGrid from "./_component/list-grid"
+import SearchForm from "./_component/search-form"
+const pageProps = {
+    title: "종합코드관리",
+    transKey: "nav.stnd.stnd0005",
+    desc: "종합코드관리",
+    url: "stnd0005"
+}
 
-export default function Page() {
+//탐색경로 설정
+const brcmp = [
+    { title: "Home", url: "/", last: false },
+    { title: "STND", url: "/", last: false },
+    { title: pageProps.title, url: pageProps.url, last: true },
+]
+
+const Stnd0005: React.FC = () => {
+
     return (
         <>
-            stnd0005
+            <PageTitle title={pageProps.title} brcmp={brcmp} />
+            <SearchForm/>
+            <ListGrid listItem={null}/>
         </>
-    );
+    )
+
 }
+
+export default Stnd0005
--- apps/kream/src/app/stnd/stnd0006/page.tsx
+++ apps/kream/src/app/stnd/stnd0006/page.tsx
@@ -2,9 +2,9 @@ import PageTitle from "../../../shared/tmpl/page-title"
 import ListGrid from "./_component/list-grid"
 import SearchForm from "./_component/search-form"
 const pageProps = {
-    title: " 언어관리",
-    transKey: "nav.stnd.stnd0004",
-    desc: "언어관리",
+    title: "차지코드관리",
+    transKey: "nav.stnd.stnd0006",
+    desc: "차지코드관리",
     url: "stnd0004"
 }
 
@@ -15,7 +15,7 @@ const brcmp = [
     { title: pageProps.title, url: pageProps.url, last: true },
 ]
 
-const Stnd0004: React.FC = () => {
+const Stnd0006: React.FC = () => {
 
     return (
         <>
@@ -27,4 +27,4 @@ const Stnd0004: React.FC = () => {
 
 }
 
-export default Stnd0004
+export default Stnd0006
