401af8287d28a9526a465a0556ede016545c3e71
doni.lee <doni.lee@kwe.com>
2024-03-08 16:58:57 +0900

    사이드바 메뉴아이콘 수정

--- apps/kream/src/states/useNavigation.tsx
+++ apps/kream/src/states/useNavigation.tsx
@@ -3,125 +3,108 @@ import { create } from 'zustand';
 import { useUserSettings } from "states/useUserSettings";
 
 const { log } = require('@repo/kwe-lib/components/logHelper');
-const { executFunction }  =  require('services/api.services');
+const { executFunction } = require('services/api.services');
 
+//DEFAULT
+import { MdOutlineWidthNormal } from "react-icons/md"
+
+//기준정보
+import { PiUserListBold  } from "react-icons/pi";
+import { TbUserPentagon } from "react-icons/tb";
+
+//ACCOUNT
+import { FcMoneyTransfer } from "react-icons/fc";
+import { BiMoneyWithdraw } from "react-icons/bi";
+import { PiHandCoinsBold } from "react-icons/pi";
+import { LiaCoinsSolid } from "react-icons/lia";
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
+
+//AIR EXPORT/IMPORT
 import {
-    FiCheckCircle,
-    FiCompass,
-    FiEye,
-    FiFileText,
-    FiHelpCircle,
-    FiList,
-    FiLogIn,
-    FiLogOut,
-    FiMove,
-    FiShare2,
-    FiShoppingBag,
-    FiTool,
-    FiTrello,
-    FiTruck,
-    FiVolume1,
-  } from "react-icons/fi";
+  PiAirplaneTakeoffBold,
+  PiAirplaneLandingBold
+} from "react-icons/pi";
+
+//계약관리
+import { GrDocumentConfig } from "react-icons/gr";
+import { HiDocumentSearch } from "react-icons/hi";
 
 export type NavigationState = {
-    parent_seq : number;
-    menu_seq : number;
-    title: string;
-    url?: string | undefined;
-    items: NavigationState[];
-    icon?: React.ReactNode;
-    menu_param?: string
-    badge?: {
-        color: string;
-        text: string | number;
-    };
+  parent_seq: number;
+  menu_seq: number;
+  title: string;
+  url?: string | undefined;
+  items: NavigationState[];
+  icon?: React.ReactNode;
+  menu_param?: string
+  badge?: {
+    color: string;
+    text: string | number;
+  };
 };
 
 // Define the initial state using that type
 export type NavigationStore = {
-    navigation: NavigationState[];
-    openMenu: string;
-    isOpen: boolean,
-    actions: {
-        setOpenMenu: (menuName?: string) => void;
-    }
-    childern?: JSX.Element|JSX.Element[]
+  navigation: NavigationState[];
+  openMenu: string;
+  isOpen: boolean,
+  actions: {
+    setOpenMenu: (menuName?: string) => void;
+  }
+  childern?: JSX.Element | JSX.Element[]
 }
 
-function getIcon(img_index:any) {
-  switch(img_index)
-  {
+function getIcon(img_index: any) {
+  switch (img_index) {
     case "0":
-      return <FiCheckCircle size={20} />
-    case "1":
-      return <FiCompass size={20} />
-    case "2":
-      return <FiEye size={20} />
+      return <TbUserPentagon  size={20} />
     case "3":
-      return <FiFileText size={20} />
-    case "4":
-      return <FiHelpCircle size={20} />
-    case "5":
-      return <FiList size={20} />
-    case "6":
-      return <FiLogOut size={20} />
-    case "7":
-      return <FiMove size={20} />
-    case "8":
-      return <FiShare2 size={20} />
-    case "9":
-      return <FiTool size={20} />
-    case "10":
-      return <FiTrello size={20} />
+      return <PiHandCoinsBold size={20} />
     case "11":
-      return <FiTruck size={20} />
-    case "12":
-      return <FiVolume1 size={20} />
+      return <GrDocumentConfig size={20}/>
     case "13":
-      return <FiTrello size={20} />
-    case "14":
-      return <FiLogIn size={20} />
+      return <PiAirplaneLandingBold size={20} />
     default:
-      return <FiShoppingBag size={20} />
+      return <MdOutlineWidthNormal size={20} />
   }
 }
 
 
 
-async function getMenuList (userInfo:any) {
+async function getMenuList(userInfo: any) {
 
-    // console.log("getMenuList :", userInfo.permission_id);
+  // console.log("getMenuList :", userInfo.permission_id);
 
-    // const inparam = ['in_permission_id','in_menu_type', 'in_user_id', 'in_ipaddr'];
-    // const invalue = [userInfo.permission_id, 'UI', userInfo.user_id, '1.2.3.4'];
-    // const inproc = 'public.f_admn_get_menulist';
-    
-    const params = {
-      inparam: ['in_permission_id','in_menu_type', 'in_user_id', 'in_ipaddr'],
-      invalue: [userInfo.permission_id, 'UI', userInfo.user_id, '1.2.3.4'],
-      inproc: 'public.f_admn_get_menulist',
-      isShowLoading: false
-    };
-    log('params', params)
-    const menus = await executFunction(params)
-    
-    log("menus", menus);
+  // const inparam = ['in_permission_id','in_menu_type', 'in_user_id', 'in_ipaddr'];
+  // const invalue = [userInfo.permission_id, 'UI', userInfo.user_id, '1.2.3.4'];
+  // const inproc = 'public.f_admn_get_menulist';
 
-    const navigationData: NavigationState[] = [{
-        parent_seq: -1,
-        menu_seq: -2,
-        title: '',
-        items: [],
-      }];
+  const params = {
+    inparam: ['in_permission_id', 'in_menu_type', 'in_user_id', 'in_ipaddr'],
+    invalue: [userInfo.permission_id, 'UI', userInfo.user_id, '1.2.3.4'],
+    inproc: 'public.f_admn_get_menulist',
+    isShowLoading: false
+  };
+  log('params', params)
+  const menus = await executFunction(params)
 
-    const menuMap = new Map<number, NavigationState>();
+  log("menus", menus);
 
-    menus[0].forEach((menu: any) => {
+  const navigationData: NavigationState[] = [{
+    parent_seq: -1,
+    menu_seq: -2,
+    title: '',
+    items: [],
+  }];
+
+  const menuMap = new Map<number, NavigationState>();
+
+  menus[0].forEach((menu: any) => {
     if (menu.use_yn === "Y") {
       const menuItem: NavigationState = {
         parent_seq: menu.parent_seq,
         menu_seq: menu.menu_seq,
-        url: menu.menu_code === '' ? "/" : `/${menu.menu_code.substring(0,4).toLowerCase()}/${menu.menu_code.toLowerCase()}`,
+        url: menu.menu_code === '' ? "/" : `/${menu.menu_code.substring(0, 4).toLowerCase()}/${menu.menu_code.toLowerCase()}`,
         icon: menu.parent_seq === '0' ? getIcon(menu.image_index) : undefined,
         title: menu.menu_name,
         items: [],
@@ -144,25 +127,24 @@ async function getMenuList (userInfo:any) {
     (menuItem) => menuItem.parent_seq == 0
   );
 
-    return navigationData;    
+  return navigationData;
 }
 
 const useNavigationStore = create<NavigationStore>((set) => ({
-    navigation: [],
-    openMenu: '',
-    isOpen: false,
-    actions: {
-      setOpenMenu: (menuName) => {
-        set({ openMenu: menuName });
-      },
+  navigation: [],
+  openMenu: '',
+  isOpen: false,
+  actions: {
+    setOpenMenu: (menuName) => {
+      set({ openMenu: menuName });
     },
-  }));
-  export const useNavigation = useNavigationStore;
-  
-  export const setNavigationData = async () => {
-    const userInfo = useUserSettings.getState().data;
-    //log("userInfo : ",userInfo);
-    const navigationData = await getMenuList(userInfo);
-    useNavigationStore.setState({ navigation: navigationData });
-  };
-  
+  },
+}));
+export const useNavigation = useNavigationStore;
+
+export const setNavigationData = async () => {
+  const userInfo = useUserSettings.getState().data;
+  //log("userInfo : ",userInfo);
+  const navigationData = await getMenuList(userInfo);
+  useNavigationStore.setState({ navigation: navigationData });
+};
