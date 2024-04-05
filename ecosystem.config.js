38af17d1c2dc2f2a8360e30750bf132b50a5550d
stephen.lim <stephen.lim@kwe.com>
2024-04-05 09:54:22 +0900

    333

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -52,7 +52,7 @@ module.exports = {
       repo : 'git@gitlab.kwe.co.kr:sdd_it/kream_web.git',
       path : '/home/sdd_it/KREAM_Web',
       ssh_options: ['StrictHostKeyChecking=no'],
-      'post-deploy' : 'pnpm install && pnpm build --filter api && pm2 reload ecosystem.config.js --version 0.1.2 --env production --only api',
+      'post-deploy' : 'pnpm install && pnpm build --filter api && pm2 reload ecosystem.config.js --env production --only api',
     },
     web_prod : {
       key : './id_rsa-api-prod', 
