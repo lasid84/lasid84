21a72daeff62d52a5dc168111a3e92075f30eb96
stephen.lim <stephen.lim@kwe.com>
2024-04-05 16:53:36 +0900

    Revert "ㅠㅠㅠ"
    This reverts commit 7e0505d44cefd5a908feb941b117439ea2f3c422.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,16 @@
 module.exports = {
+<<<<<<< HEAD
   apps : [{
       name: 'api',
       script: 'node index.js',
       cwd: "./apps/api/dist",
+=======
+  apps : [
+    {
+      name: 'kwe_api',
+      script: 'node /home/sdd_it/KREAM_Web/source/apps/api/dist/index.js',
+      // cwd: "",
+>>>>>>> parent of 7e0505d (ㅠㅠㅠ)
       autorestart: true,
       watch: '.',
       env: {
@@ -42,10 +50,14 @@ module.exports = {
       repo : 'git@gitlab.kwe.co.kr:sdd_it/kream_web.git',
       path : '/home/sdd_it/KREAM_Web',
       ssh_options: ['StrictHostKeyChecking=no'],
-      'post-deploy' : 'pnpm install && pnpm build --filter kwe-api && pm2 reload ecosystem.config.js --only kwe-api',
+      'post-deploy' : 'pnpm install && pnpm build --filter api && pm2 reload ecosystem.config.js --only api',
     },
     api_prod : {
+<<<<<<< HEAD
       name: 'api',
+=======
+      name: 'kwe_api',
+>>>>>>> parent of 7e0505d (ㅠㅠㅠ)
       key : './id_rsa-api-prod', 
       user : 'sdd_it',
       host : ['10.33.63.171'],
@@ -53,7 +65,11 @@ module.exports = {
       repo : 'git@gitlab.kwe.co.kr:sdd_it/kream_web.git',
       path : '/home/sdd_it/KREAM_Web',
       ssh_options: ['StrictHostKeyChecking=no'],
+<<<<<<< HEAD
       'post-deploy' : 'pnpm install && pnpm build --filter api && pm2 reload ecosystem.config.js --only api',
+=======
+      'post-deploy' : 'pnpm install && pnpm build --filter kwe_api && pm2 reload ecosystem.config.js --only kwe_api',
+>>>>>>> parent of 7e0505d (ㅠㅠㅠ)
     },
     // batch_prod : {
     //   key : './id_rsa-batch-prod', 
