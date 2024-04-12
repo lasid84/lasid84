2c497a89d7783b11e92c831caf368b9e4e2a9542
stephen.lim <stephen.lim@kwe.com>
2024-04-12 13:40:13 +0900

    44

--- apps/kream/next.config.mjs
+++ apps/kream/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // i18n: null,
+    distDir: 'dist',
     reactStrictMode: false,
     // experimental: {
     //     missingSuspenseWithCSRBailout: false,
