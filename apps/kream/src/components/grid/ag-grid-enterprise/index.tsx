caa4647753eccf6fd2c940c59c823dcdfb975f4d
stephen.lim <stephen.lim@kwe.com>
2024-06-12 10:34:58 +0900

    22

--- apps/kream/src/components/grid/ag-grid-enterprise/index.tsx
+++ apps/kream/src/components/grid/ag-grid-enterprise/index.tsx
@@ -17,7 +17,7 @@ import {
 } from "ag-grid-community";
 
 import { LicenseManager } from  'ag-grid-enterprise'
-LicenseManager.setLicenseKey(process.env.AG_GRID_LICENSE + '');
+LicenseManager.setLicenseKey('Using_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-061191}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{KWE_Korea}_is_granted_a_{Single_Application}_Developer_License_for_the_application_{KREAM}_only_for_{2}_Front-End_JavaScript_developers___All_Front-End_JavaScript_developers_working_on_{KREAM}_need_to_be_licensed___{KREAM}_has_been_granted_a_Deployment_License_Add-on_for_{1}_Production_Environment___This_key_works_with_{AG_Charts_and_AG_Grid}_Enterprise_versions_released_before_{5_June_2025}____[v3]_[0102]_MTc0OTA3ODAwMDAwMA==696bfcd61d61d6c5c46fd62e57be170b');
 
 import { crudType, useAppContext } from "components/provider/contextProvider";
 import { useTranslation } from 'react-i18next';
