4723a47bd21111e7eb80058a2d7861761438a2c9
stephen.lim <stephen.lim@kwe.com>
2024-06-18 16:40:17 +0900

    ※ 배포 서버 정리

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -37,19 +37,9 @@ module.exports = {
   ],
 
   deploy : {
-    api_staging : {
-      key : './id_rsa', 
-      user : 'sdd_it',
-      host : ['10.33.63.171'],
-      ref  : 'origin/main',
-      repo : 'git@gitlab.kwe.co.kr:sdd_it/kream_web.git',
-      path : '/home/sdd_it/KREAM_Web',
-      ssh_options: ['StrictHostKeyChecking=no'],
-      'post-deploy' : 'pnpm install && pnpm build --filter api && pm2 reload ecosystem.config.js --only api',
-    },
-    api_prod : {
+    api_test : {
       name: 'api',
-      key : './id_rsa-api-prod', 
+      key : './id_rsa-api-test', 
       user : 'sdd_it',
       host : ['10.33.63.171'],
       ref  : 'origin/main',
@@ -68,18 +58,8 @@ module.exports = {
     //   ssh_options: ['StrictHostKeyChecking=no'],
     //   'post-deploy' : 'pnpm install && pnpm build --filter kwe-api && pm2 reload ecosystem.config.js --only kwe-api',
     // },
-    kream_prod : {
-      key : './id_rsa-kream-prod', 
-      user : 'sdd_it',
-      host : ['10.33.63.174'],
-      ref  : 'origin/main',
-      repo : 'git@gitlab.kwe.co.kr:sdd_it/kream_web.git',
-      path : '/home/sdd_it/KREAM_Web',
-      ssh_options: ['StrictHostKeyChecking=no'],
-      'post-deploy' : 'pnpm install && pnpm build --filter kream && pm2 reload ecosystem.config.js --only kream',
-    },
-    batch_prod : {
-      key : './id_rsa-batch-prod', 
+    batch_test : {
+      key : './id_rsa-batch-test', 
       user : 'sdd_it',
       host : ['10.33.63.172'],
       ref  : 'origin/main',
@@ -88,15 +68,15 @@ module.exports = {
       ssh_options: ['StrictHostKeyChecking=no'],
       'post-deploy' : 'pnpm install && pnpm build --filter batch && pm2 reload ecosystem.config.js --only batch',
     },
-    // batch_prod : {
-    //   key : './id_rsa-api-prod', 
-    //   user : 'sdd_it',
-    //   host : ['10.33.63.171'],
-    //   ref  : 'origin/main',
-    //   repo : 'git@gitlab.kwe.co.kr:sdd_it/kream_web.git',
-    //   path : '/home/sdd_it/KREAM_Web',
-    //   ssh_options: ['StrictHostKeyChecking=no'],
-    //   'post-deploy' : 'pnpm install && pnpm build --filter batch && pm2 reload ecosystem.config.js --only batch',
-    // }
+    kream_test : {
+      key : './id_rsa-kream-test', 
+      user : 'sdd_it',
+      host : ['10.33.63.174'],
+      ref  : 'origin/main',
+      repo : 'git@gitlab.kwe.co.kr:sdd_it/kream_web.git',
+      path : '/home/sdd_it/KREAM_Web',
+      ssh_options: ['StrictHostKeyChecking=no'],
+      'post-deploy' : 'pnpm install && pnpm build --filter kream && pm2 reload ecosystem.config.js --only kream',
+    },
   }
 };
