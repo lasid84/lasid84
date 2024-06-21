11aaf9b42767fee7b220d85248f99d7ce72fb287
stephen.lim <stephen.lim@kwe.com>
2024-06-21 17:35:56 +0900

    10

--- apps/kream/next.config.mjs
+++ apps/kream/next.config.mjs
@@ -1,59 +1,15 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     // i18n: null,
-//     reactStrictMode: false,
-//     // experimental: {
-//     //     missingSuspenseWithCSRBailout: false,
-//     // },
-//     experimental: {
-//    	 serverActions: {
-// 	      allowedOrigins: ["dev-kream.web.kwe.co.kr", "dev-api-kream.web.kwe.co.kr", "localhost:3000"]
-// 	    }
-// 	  }
-
-// };
-// export default nextConfig;
-
-const headers = [
-  "Accept", "Accept-Version", "Content-Length",
-  "Content-MD5", "Content-Type", "Date", "X-Api-Version",
-  "X-CSRF-Token", "X-Requested-With",
-];
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, 
-  experimental: {
-    serverActions: {
-        allowedOrigins: ["dev-kream.web.kwe.co.kr", "dev-api-kream.web.kwe.co.kr", "localhost:3000"]
-      }
-  },
-  env: {
-    ALLOWED_NEXT_AUTH_URLS:
-    ["http://(.+\\.|)dev-kream.web.kwe.co.kr/?","http://(.+\\.|)dev-api-kream.web.kwe.co.kr/?","http://(.+\\.|)localhost:3000/?"], ALLOWED_HEADERS:
-    headers.join(", "), CORS_DEFAULTS: {
-      methods: [], // making this blank by default - you have to override it per-call
-      origin: "*",
-      allowedHeaders: headers.join(", "),
-      credentials: true,
-    },
-  }, async headers() {
-    return [
-      {
-	source: "/api/(.*)", headers: [
-	  { key: "Access-Control-Allow-Credentials", value:
-	  "true" }, { key: "Access-Control-Allow-Origin",
-	  value: "*" }, {
-	    key: "Access-Control-Allow-Methods", value:
-	    "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-	  }, {
-	    key: "Access-Control-Allow-Headers", value:
-	    headers.join(", "),
-	  },
-	],
-      },
-    ];
-  },
+    // i18n: null,
+    reactStrictMode: false,
+    // experimental: {
+    //     missingSuspenseWithCSRBailout: false,
+    // },
+    experimental: {
+   	 serverActions: {
+	      allowedOrigins: ["dev-kream.web.kwe.co.kr", "dev-api-kream.web.kwe.co.kr", "localhost:3000"]
+	    }
+	  }
 
 };
 export default nextConfig;
@@ -64,10 +20,17 @@ export default nextConfig;
 //   "X-CSRF-Token", "X-Requested-With",
 // ];
 
-// module.exports = {
-//   reactStrictMode: true, env: {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true, 
+//   experimental: {
+//     serverActions: {
+//         allowedOrigins: ["dev-kream.web.kwe.co.kr", "dev-api-kream.web.kwe.co.kr", "localhost:3000"]
+//       }
+//   },
+//   env: {
 //     ALLOWED_NEXT_AUTH_URLS:
-//     ["https://(.+\\.|)dev-kream.web.kwe.co.kr/?","https://(.+\\.|)dev-api-kream.web.kwe.co.kr/?"], ALLOWED_HEADERS:
+//     ["http://(.+\\.|)dev-kream.web.kwe.co.kr/?","http://(.+\\.|)dev-api-kream.web.kwe.co.kr/?","http://(.+\\.|)localhost:3000/?"], ALLOWED_HEADERS:
 //     headers.join(", "), CORS_DEFAULTS: {
 //       methods: [], // making this blank by default - you have to override it per-call
 //       origin: "*",
@@ -91,4 +54,41 @@ export default nextConfig;
 //       },
 //     ];
 //   },
+
 // };
+// export default nextConfig;
+
+// // const headers = [
+// //   "Accept", "Accept-Version", "Content-Length",
+// //   "Content-MD5", "Content-Type", "Date", "X-Api-Version",
+// //   "X-CSRF-Token", "X-Requested-With",
+// // ];
+
+// // module.exports = {
+// //   reactStrictMode: true, env: {
+// //     ALLOWED_NEXT_AUTH_URLS:
+// //     ["https://(.+\\.|)dev-kream.web.kwe.co.kr/?","https://(.+\\.|)dev-api-kream.web.kwe.co.kr/?"], ALLOWED_HEADERS:
+// //     headers.join(", "), CORS_DEFAULTS: {
+// //       methods: [], // making this blank by default - you have to override it per-call
+// //       origin: "*",
+// //       allowedHeaders: headers.join(", "),
+// //       credentials: true,
+// //     },
+// //   }, async headers() {
+// //     return [
+// //       {
+// // 	source: "/api/(.*)", headers: [
+// // 	  { key: "Access-Control-Allow-Credentials", value:
+// // 	  "true" }, { key: "Access-Control-Allow-Origin",
+// // 	  value: "*" }, {
+// // 	    key: "Access-Control-Allow-Methods", value:
+// // 	    "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+// // 	  }, {
+// // 	    key: "Access-Control-Allow-Headers", value:
+// // 	    headers.join(", "),
+// // 	  },
+// // 	],
+// //       },
+// //     ];
+// //   },
+// // };
