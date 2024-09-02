311085482b8cdf6278879272453e95ac8770e54a
stephen.lim <stephen.lim@kwe.com>
2024-09-02 15:41:49 +0900

    -

--- apps/batch/components/api.service/api.service.js
+++ apps/batch/components/api.service/api.service.js
@@ -13,21 +13,21 @@ var iniData;
 var url;
 var token;
 
-async function initService() {
+// async function initService() {
     
-    //개발서버에서는 /dist까지 나와서 임시로 아래와 같이 설정 - stephen
-    let root = process.cwd();
-    if (root.endsWith("/dist")) root = root.replace("/dist", "");
+//     //개발서버에서는 /dist까지 나와서 임시로 아래와 같이 설정 - stephen
+//     let root = process.cwd();
+//     if (root.endsWith("/dist")) root = root.replace("/dist", "");
 
-    if (!url) {
-        //batch 프로젝트는 process.cwd() : /home/sdd_it/kream_web/apps/batch/dist 임.. 왜 api랑 다른지 모르겠음
-        iniData = ini.decode(await fs.readFile(root + "/dist/configs/server.ini", "utf8"));
-        url = objectPath.get(iniData, "main.url");
-        // error("initService", url, token, iniData);
-    }
-}
+//     if (!url) {
+//         //batch 프로젝트는 process.cwd() : /home/sdd_it/kream_web/apps/batch/dist 임.. 왜 api랑 다른지 모르겠음
+//         iniData = ini.decode(await fs.readFile(root + "/dist/configs/server.ini", "utf8"));
+//         url = objectPath.get(iniData, "main.url");
+//         // error("initService", url, token, iniData);
+//     }
+// }
 
-async function initService2() {
+async function initService() {
     url = process.env.API_URL;
     // log("process.env.API_URL : ", url);
 }
@@ -57,7 +57,7 @@ async function executFunction(inproc, inparam, invalue) {
 
 async function sendEmail(mailOptions) {
 
-    await initService2();
+    await initService();
     token = signJwtAccessToken({user_id:"sdd_it", user_nm:"SDD"});
     var config = {
         type: "1",
