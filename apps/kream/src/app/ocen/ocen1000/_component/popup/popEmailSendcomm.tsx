e5ecb391fe5fb40b177de0630966a46bdcc48b86
doni.lee <doni.lee@kwe.com>
2024-09-30 15:47:08 +0900

    2024.09.30 1)불필요한코드제거 2)버그수정

--- apps/kream/src/app/ocen/ocen1000/_component/popup/popEmailSendcomm.tsx
+++ apps/kream/src/app/ocen/ocen1000/_component/popup/popEmailSendcomm.tsx
@@ -205,6 +205,7 @@ const Modal: React.FC<Props> = ({
     setCustCode('')
     setPgmCode(TRANPOSRT_EMAIL_LIST_OE)
     setTemplateTypeList([]);
+    setIsClicked(false)
   };
 
   const handleFileDrop = async (data: any[], header: ArrayBuffer[]) => {
@@ -238,6 +239,7 @@ const Modal: React.FC<Props> = ({
   const sendTransPortEmail = useCallback(async () => {
     const curData = getValues();
     setIsClicked(true); // 중복클릭방지 state 추가
+
     const fileUploadRequest : FileUploadRequest = {
       addFolderName : user_id,
       files : []
@@ -246,9 +248,7 @@ const Modal: React.FC<Props> = ({
     if (templateTypeList.length > 0) {
 
       let reportList = [];
-        if (reports.length === templateTypeList.length) {
-          reportList.push(reports);
-        } else {
+
           for (const report of reports) {
             for (const template of templateTypeList) {
               if (report.key === template) {
@@ -256,7 +256,6 @@ const Modal: React.FC<Props> = ({
               }
             }
           }
-        }
 
         /**
          * @dev
@@ -269,7 +268,6 @@ const Modal: React.FC<Props> = ({
         
         for (const report of reportList) {
             try {
-
               await GetReportData.mutateAsync({ type: (report.report_type-1), bk_id: bk_id }, {
                 onSuccess: (res:any) => {
                   let reportData : any = new Object;
@@ -295,7 +293,7 @@ const Modal: React.FC<Props> = ({
                   reportDataList.push(reportData);        
                 },
                 onError: (error) => {
-                  console.error(` 실패 (type: ${report.key}):`, error);
+                  log(` 실패 (type: ${report.key}):`, error);
                 }
               });
             } catch (error) {
@@ -326,9 +324,8 @@ const Modal: React.FC<Props> = ({
 
             res.data.forEach((data: any, index: number) => {
 
-              // fileNameList와 res.data가 함께 움직임
               const files: FileUploadData = {
-                fileName: fileNameList[index]+data.extension,  // 각 인덱스에 맞는 파일 이름 할당
+                fileName: fileNameList[index]+data.extension,  
                 fileData: data.fileData,
                 fileRootDIR: "MAIL_ATTACH"
               };
