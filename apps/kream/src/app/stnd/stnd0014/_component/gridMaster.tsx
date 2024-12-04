d086d8f60822e1d8c1eca2de155707450342ce8e
stephen.lim <stephen.lim@kwe.com>
2024-12-04 10:29:19 +0900

    -코드 정리

--- apps/kream/src/app/stnd/stnd0014/_component/gridMaster.tsx
+++ apps/kream/src/app/stnd/stnd0014/_component/gridMaster.tsx
@@ -30,7 +30,7 @@ const MasterGrid: React.FC<Props> = memo(({ initData }) => {
 
 
     const gridOption: GridOption = {
-        colVisible: { col: ["day_of_week", "leap_month", "lucky_day", "use_yn"], visible: false },
+        colVisible: { col: ["use_yn"], visible: false },
         editable: ["day_nm", "day_off", "remark"],
         gridHeight: "h-full",
         checkbox: ["day_off"],
